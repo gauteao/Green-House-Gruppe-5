4d64a2074e36779364a3a450146a904644fab3dd
Jerezard <jeremys-1999@hotmail.com>
2023-12-09 14:28:01 +0100

    New branch update, will merge witmain branch

---
+++ src/main/java/no/ntnu/greenhouse/ClientHandler.java
@@ -0,0 +1,15 @@
+package no.ntnu.greenhouse;
+import java.io.*;
+import java.net.*;
+
+public class ClientHandler implements Runnable {
+
+    @Override
+    public void run() {
+            
+    }
+
+
+
+
+}
